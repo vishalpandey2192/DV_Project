ce5dd8bb3830dada9bfff8f4a63fefa0c6511cc7
Vishal Pandey <vishal.pandey@utah.edu>
2017-11-09 21:43:24 -0700

    Project Milestone

---
+++ js/map.js
@@ -0,0 +1,196 @@
+/** Class implementing the map view. */
+class Map {
+    /**
+     * Creates a Map Object
+     */
+    constructor(allData) {
+        //Width and height of map
+        this.width = 760;
+        this.height = 500;
+        this.data = allData
+
+
+        //Create SVG element and append map to the SVG
+        this.svg = d3.select("#map-view")
+            .append("svg")
+            .attr("width", this.width)
+            .attr("height", this.height);
+
+    }
+
+    updateMap(option) {
+
+        var value = this.getCSVPropertyName(option)
+        var col = "rgb(217,91,67)"
+        var yMin = d3.min(this.data, function(d) {
+            return d[value];
+        })
+        var yMax = d3.max(this.data, function(d) {
+            return d[value];
+        })
+
+        var color = d3.scaleLinear()
+            .domain([yMin, yMax])
+            .range([d3.rgb(col).brighter(), d3.rgb(col).darker()]);
+
+        //adding tooltip
+        var tooltip = d3.select("body").append("div")
+            .attr("class", "tooltip-title")
+            .style("opacity", 0);
+
+        for (var i = 0; i < this.data.length; i++) {
+
+            // Grab State Name
+            var dataState = this.data[i].RegionName;
+
+            // Grab data value
+            var dataValue = this.data[i][value];
+
+            // Find the corresponding state inside the GeoJSON
+            for (var j = 0; j < this.statesJson.features.length; j++) {
+                var jsonState = this.statesJson.features[j].properties.name;
+
+                if (dataState == jsonState) {
+
+                    // Copy the data value into the JSON
+                    this.statesJson.features[j].properties[value] = dataValue;
+
+                    // Stop looking through the JSON
+                    break;
+                }
+            }
+        }
+
+        // Bind the data to the SVG and create one path per GeoJSON feature
+        var self = this;
+        this.svg.selectAll("path")
+            .data(this.statesJson.features)
+            .enter()
+            .append("path")
+            .attr("d", this.path)
+            .style("stroke", "#fff")
+            .style("stroke-width", "1")
+            .style("fill", function(d) {
+
+                // Get data value
+                var val = d.properties[value];
+
+                if (val) {
+                    return color(val);
+                } else {
+                    return "rgb(213,222,217)";
+                }
+            })
+            .on("click", function(d) {
+
+                for (i = 0; i < self.data.length; i++) {
+                    if (self.data[i]['RegionName'].toLowerCase() === d.properties["name"].toLowerCase()) {
+                        var stateData = self.data[i]
+                        createTableForClickedState(stateData)
+                    }
+                }
+
+            })
+            .on("mouseout", function(d) {
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", 0);
+            })
+            .on("mouseover", function(d) {
+
+                for (i = 0; i < self.data.length; i++) {
+                    if (self.data[i]['RegionName'].toLowerCase() === d.properties["name"].toLowerCase()) {
+                        var stateData = self.data[i]
+                        var tooltip_data = {
+                            "state": stateData["RegionName"],
+                            "price": stateData["PRICE/FT."],
+                            "population": stateData["POPULATION"],
+                            "unemployement": stateData["UNEMPLOYMENT_RATE"],
+                            "salary": stateData["AVERAGE_SALARY/MON"],
+                            "mortality": stateData["MORTALITY_RATE"]
+                        }
+                        var body = self.tooltip_render(tooltip_data)
+                        tooltip.transition()
+                            .duration(200)
+                            .style("opacity", .9);
+                        tooltip.html(
+                            body
+                        )
+                        tooltip.style("left", (d3.event.pageX) + "px")
+                        tooltip.style("top", (d3.event.pageY) + "px");
+                        window.barChart.highlightState(self.data[i]['RegionName'])
+                    }
+                }
+            })
+
+
+
+        var labels = this.svg.append('g').attr('class', 'labels');
+        var path = this.path
+        labels.selectAll('.label').data(this.statesJson.features).enter().append('text')
+            .attr("class", "label")
+            .attr("x", function(d) {
+                return path.centroid(d)[0];
+            })
+            .attr("y", function(d) {
+                return path.centroid(d)[1];
+            })
+            .style('text-anchor', 'middle')
+            .text(function(d) {
+                return d.properties.name
+            })
+
+
+    }
+
+    /**
+     * Renders the actual map
+     * @param the json data with the shape of all countries
+     */
+    drawMap(json, value) {
+
+        this.statesJson = json;
+        // D3 Projection
+        this.projection = d3.geoAlbersUsa()
+            .translate([this.width / 2, this.height / 2]) // translate to center of screen
+            .scale([1000]); // scale things down so see entire US
+
+        // Define path generator
+        this.path = d3.geoPath() // path generator that will convert GeoJSON to SVG paths
+            .projection(this.projection); // tell path generator to use albersUsa projection
+        this.updateMap(value)
+    }
+
+    getCSVPropertyName(value) {
+        if (value == 'unemployement') {
+            return 'UNEMPLOYMENT_RATE'
+        }
+        if (value == 'salary') {
+            return 'AVERAGE_SALARY/MON'
+        }
+        if (value == 'population') {
+            return 'POPULATION'
+        }
+        if (value == 'price') {
+            return 'PRICE/FT.'
+        }
+        if (value == 'mortality') {
+            return 'MORTALITY_RATE'
+        }
+
+    }
+
+    tooltip_render(tooltip_data) {
+
+        var text = "<h5>" + tooltip_data.state + "</h5>";
+        text += "<ul>"
+        text += "<li> <strong>Population:</strong> " + tooltip_data.population + "</li>"
+        text += "<li> <strong> Price per foot:</strong> " + tooltip_data.price + "</li>"
+        text += "<li> <strong>Mortality Rate:</strong> " + tooltip_data.mortality + "</li>"
+        text += "<li> <strong>Average Salary:</strong> " + tooltip_data.salary + "</li>"
+        text += "<li> <strong>Unemployement Rate:</strong> " + tooltip_data.unemployement + "</li>"
+        text += "</ul>";
+
+        return text;
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,165 @@
+var usaMap;
+
+d3.csv("2016_us_states_data.csv", function(error, allData) {
+
+    allData.forEach(function(d) {
+        // Convert numeric values to 'numbers'
+        d['POPULATION'] = +d['POPULATION'];
+        d['UNEMPLOYMENT_RATE'] = +d['UNEMPLOYMENT_RATE'];
+        d['AVERAGE_SALARY/MON'] = +d['AVERAGE_SALARY/MON'];
+        d['MORTALITY_RATE'] = +d['MORTALITY_RATE'];
+        d['PRICE/FT.'] = +d['PRICE/FT.'];
+    });
+
+    usaMap = new Map(allData);
+    window.barChart = new BarChart(allData);
+
+
+    d3.json("us-state-centroid.json", function(json) {
+        usaMap.drawMap(json, 'unemployement');
+        // Draw the Bar chart for the first time
+        barChart.updateBarChart('unemployement');
+    });
+
+});
+
+function updateMap(value) {
+    var map = d3.select("#map-view").selectAll("path");
+    map.remove().exit();
+    d3.select("#bar-chart").selectAll("rect").classed("highlight-class", false)
+    document.getElementById('table').innerHTML = '';
+    usaMap.updateMap(value)
+    barChart.updateBarChart(value);
+}
+
+function createTableForClickedState(selectedState) {
+    var body = document.getElementById("table");
+    body.innerHTML = '';
+
+    // creates a <table> element and a <tbody> element
+    var tbl = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+    var row = document.createElement("tr");
+    var cell = document.createElement("td");
+    cell.setAttribute("class", "bold");
+    var cellText = document.createTextNode("Parameters");
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    var cell = document.createElement("td");
+    cell.setAttribute("class", "bold");
+    var cellText = document.createTextNode(selectedState["RegionName"]);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    var cell = document.createElement("td");
+    cell.setAttribute("class", "bold");
+    var cellText = document.createTextNode("National Average");
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    tblBody.appendChild(row);
+
+    row = document.createElement("tr");
+    cell = document.createElement("td");
+    cell.setAttribute("class", "bold");
+
+    cellText = document.createTextNode("Price Per sq. foot ($/foot.)");
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    cell = document.createElement("td");
+    cellText = document.createTextNode(selectedState["PRICE/FT."]);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    cell = document.createElement("td");
+    cellText = document.createTextNode(selectedState["NA PRICE/FT."]);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    tblBody.appendChild(row);
+
+    row = document.createElement("tr");
+    cell = document.createElement("td");
+    cell.setAttribute("class", "bold");
+    cellText = document.createTextNode("Mortality Rate (%)");
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    cell = document.createElement("td");
+    cellText = document.createTextNode(selectedState["MORTALITY_RATE"]);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    cell = document.createElement("td");
+    cellText = document.createTextNode(selectedState["NA MORTALITY_RATE"]);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    tblBody.appendChild(row);
+
+    row = document.createElement("tr");
+    cell = document.createElement("td");
+    cell.setAttribute("class", "bold");
+    cellText = document.createTextNode("Population");
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    cell = document.createElement("td");
+    cellText = document.createTextNode(selectedState["POPULATION"]);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    cell = document.createElement("td");
+    cellText = document.createTextNode(selectedState["NA POPULATION"]);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    tblBody.appendChild(row);
+
+    row = document.createElement("tr");
+    cell = document.createElement("td");
+    cell.setAttribute("class", "bold");
+    cellText = document.createTextNode("Unemployement Rate (%)");
+
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    cell = document.createElement("td");
+    cellText = document.createTextNode(selectedState["UNEMPLOYMENT_RATE"]);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    cell = document.createElement("td");
+    cellText = document.createTextNode(selectedState["NA UNEMPLOYMENT_RATE"]);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    tblBody.appendChild(row);
+
+    row = document.createElement("tr");
+    cell = document.createElement("td");
+    cell.setAttribute("class", "bold");
+
+    cellText = document.createTextNode("Average Salary ($)");
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    cell = document.createElement("td");
+    cellText = document.createTextNode(selectedState["AVERAGE_SALARY/MON"]);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    cell = document.createElement("td");
+    cellText = document.createTextNode(selectedState["NA AVERAGE_SALARY/MON"]);
+    cell.appendChild(cellText);
+    row.appendChild(cell);
+
+    tblBody.appendChild(row);
+
+    tbl.appendChild(tblBody);
+    body.appendChild(tbl);
+    tbl.setAttribute("border", "2");
+    tbl.setAttribute("class", "table-class table")
+}
